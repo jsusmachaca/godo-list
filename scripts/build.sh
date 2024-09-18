646dd39a57fa04921d63a46adee8ee3b1b1cad9e
jsusmachaca <falcorgd@gmail.com>
2024-09-18 15:09:38 -0500

    feat: update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,24 +2,35 @@
 
 ARCH=$(uname -m)
 
-if [[ $ARCH == "x86_64" ]]; then
-    GOARCH="amd64"
-elif [[ $ARCH == "i686" || $ARCH == "i386" ]]; then
-    GOARCH="386"
-else
-    echo "Arquitectura no soportada: $ARCH"
+case $ARCH in
+    x86_64)
+        GOARCH="amd64"
+        ;;
+    i686|i386)
+        GOARCH="386"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+if ! GOARCH=$GOARCH GOOS=linux go build cmd/web/main.go; then
+    echo "Build failed"
     exit 1
 fi
 
-GOARCH=$GOARCH GOOS=linux go build cmd/web/main.go
+SRC_DIR="src"
+TEMPLATE_DIR="web/template"
+STATIC_DIR="web/static"
 
-mkdir -p src/web/template
-mkdir -p src/web/static
+mkdir -p $SRC_DIR/web/template
+mkdir -p $SRC_DIR/web/static
 
-cp -r web/template/* src/web/template
-cp -r web/static/* src/web/static
-mv main src
+cp -r $TEMPLATE_DIR/* $SRC_DIR/web/template
+cp -r $STATIC_DIR/* $SRC_DIR/web/static
+mv main $SRC_DIR
 
-tar -czf web.gz src 
+tar -czf webapp.tar.gz $SRC_DIR
 
-rm -rf src
+rm -rf $SRC_DIR
